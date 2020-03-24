d98ea0662c5a17e1d822b9f229e4805af902ac5d
Arshdeep Singh <rathorearsh@gmail.com>
2020-03-24 11:44:18 +0100

    ProjectCreation

---
+++ ASPwebApiTest/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ASPwebApiTest.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Entities/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Products
+    {
+        public int ID { get; set; }
+        public string product_id { get; set; }
+        public string product_name { get; set; }
+        public string stock_available { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
